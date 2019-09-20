cd6ced871179c23843f1f8c1be34af247a2e9477
Alexander Zhevak <zhevak@neurotech.lt>
2019-09-20 13:03:46 +0500

    добавлены файлы memvis.[ch]

---
+++ Inc/memvis.h
@@ -0,0 +1,20 @@
+/**
+@file    memvis.h
+@version 0.0
+@date    2019.09.20
+
+@author  alex
+@email   zhevak@mail.ru
+
+@brief   TCP/IP сервер. Передаёт содержимое памяти микроконтроллера клиенту.
+*/
+
+
+#ifndef __MEMVIS_H__
+#define __MEMVIS_H__
+
+
+
+
+
+#endif  // __MEMVIS_H__
---
+++ Src/memvis.c
@@ -0,0 +1,15 @@
+/**
+@file    memvis.c
+@version 0.0
+@date    2019.09.20
+
+@author  alex
+@email   zhevak@mail.ru
+
+@brief   TCP/IP сервер. Передаёт содержимое памяти микроконтроллера клиенту.
+*/
+
+
+#include "memvis.h"
+
+
